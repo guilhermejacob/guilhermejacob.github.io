73d73af6700bd7016ffab52ab791ada4875b5d89
guilhermejacob <guilhermejacob91@gmail.com>
2019-12-21 17:07:14 -0300

    atualiza site

---
+++ scripts/cria_datasus.R
@@ -0,0 +1,53 @@
+# cria todas as bases do datasus
+
+# define diretórios
+output_dir <- "/home/guilherme/Bases/DataSUS"
+datavault_dir <- "/home/guilherme/DataVault/DataSUS"
+
+# SIASUS
+
+# lê funções
+source("/home/guilherme/GitLab/src-website/content/private_scripts/siasus.R")
+
+# unlink( file.path( output_dir , "siasus" ), recursive = TRUE)
+
+catalog_init <- catalog_siasus( output_dir = output_dir )
+catalog_dv <- datavault_siasus( catalog = catalog_init , datavault_dir = datavault_dir )
+catalog_final <- build_siasus( catalog = catalog_dv )
+monetdb_siasus( catalog = catalog_final , skipExist = FALSE )
+
+# SINAN
+
+# lê funções
+source("/home/guilherme/GitLab/src-website/content/private_scripts/sinan.R")
+
+# unlink( file.path( output_dir , "sinan" ), recursive = TRUE)
+
+catalog_init <- catalog_sinan( output_dir = output_dir )
+catalog_dv <- datavault_sinan( catalog = catalog_init , datavault_dir = datavault_dir )
+catalog_final <- build_sinan( catalog = catalog_dv )
+monetdb_sinan( catalog = catalog_final , skipExist = FALSE )
+
+# PNI
+
+# lê funções
+source("/home/guilherme/GitLab/src-website/content/private_scripts/pni.R")
+
+# unlink( file.path( output_dir , "pni" ), recursive = TRUE)
+
+catalog_init <- catalog_pni( output_dir = output_dir )
+catalog <- catalog_dv <- datavault_pni( catalog = catalog_init , datavault_dir = datavault_dir )
+catalog_final <- build_pni( catalog = catalog_dv )
+monetdb_pni( catalog = catalog_final )
+
+# SIM, SINASC, SISPRENATAL
+
+# lê funções
+source("/home/guilherme/GitLab/src-website/content/private_scripts/datasus.R")
+
+# unlink( file.path( output_dir , c( "sim" , "sinasc" , "sisprenatal" ) ) , recursive = TRUE)
+
+catalog_init <- catalog_datasus( output_dir = output_dir )
+catalog <- catalog_dv <- datavault_datasus( catalog = catalog_init , datavault_dir = datavault_dir )
+catalog_final <- build_datasus( catalog = catalog_dv[ catalog$year >= 2000 , ] )
+monetdb_datasus( catalog = catalog_final )
---
+++ scripts/siasus.R
@@ -0,0 +1,381 @@
+# DATASUS: SIASUS.
+
+### Este arquivo contém as principais funções necessárias para
+### montar as bases de dados do SIASUS/DataSUS.
+### A idéia é construir um catálogo com a função catalog_siasus(),
+### filtrar (ou não) os dados desejados, depois montar as bases de dados
+### no disco usando a função build_siasus().
+
+### funções auxiliares
+link_scrape <- function( x ) {
+  library(RCurl , quietly = TRUE )
+  custom_recodes <- c( "í" = "%ED" , "á" = "%E1" , "ã" = "%E3" , " " = "%20" )
+  custom_recodes <- NULL
+  if ( !is.null( x ) ){
+    this_text <- getURL( x , .encoding = 'ISO-8859-1' , .opts = list(timeout = 10 , dirlistonly = TRUE , ftplistonly = TRUE , ftp.use.epsv = FALSE ) )
+    this_text <- unlist( strsplit( this_text , "\n" ) )
+    this_text <- gsub( "\\r$" , "" , this_text )
+    this_text <- curlPercentEncode( this_text , codes = custom_recodes )
+    paste0( x , this_text )
+  } else { 
+    NULL 
+  }
+}
+
+getlisting <- function( these_urls ) {
+  
+  library(future.apply)
+  plan(multiprocess)
+  
+  these_urls <- ifelse( grepl( "\\/$" , these_urls ) , these_urls , paste0( these_urls , "/" ) )
+  res <- future_lapply( these_urls , link_scrape )
+  res <- unlist( res )
+  res <- res[ !grepl( "SISPRENATAL\\/201201_\\/Doc$", res ) ] # folder not loading in server
+  is.file <- grepl( "\\." , basename( res ) , ignore.case = TRUE )
+  res[ !is.file ] <- ifelse( grepl( "\\/$" , res[ !is.file ] ) , res[ !is.file ] , paste0( res[ !is.file ] , "/" ) )
+  list( dirs = if ( any(!is.file) ) { res[ !is.file ] } else { NULL } , 
+        files = if ( any(is.file) ) { res[ is.file ] } else { NULL } )
+}
+
+recursive_ftp_scrape <- function( main_url , max.iter = Inf ) {
+  
+  final_files <- NULL
+  
+  directories <- main_url
+  
+  i=0
+  while ( length(directories) > 0 ) {
+    i=i+1
+    if ( i > max.iter ) { break() }
+    listing <- getlisting(directories)
+    directories <- listing[[1]]
+    files <- listing[[2]]
+    final_files <- c(final_files, files )
+    if ( length( directories) > 0 ) cat( length(directories) , "new directories found.\n" ) else cat( "done!\n" )
+  }
+  
+  return( final_files )
+  
+}
+
+### cria catálogo de dados
+catalog_siasus <- function( output_dir , drop.prelim = TRUE ){
+  
+  # define pastas-raízes
+  sus_htmls <- c( "ftp://ftp.datasus.gov.br/dissemin/publicos/SIASUS/200801_/" , "ftp://ftp.datasus.gov.br/dissemin/publicos/SIASUS/199407_200712/Dados/" )
+  
+  # percorre diretórios
+  these_links <- recursive_ftp_scrape( sus_htmls )
+  
+  # mantém apenas arquivos de dados
+  these_links <- these_links[ grepl( "dados.*\\.(dbc|dbf)$" , tolower(these_links) ) ]
+  
+  # cria catálogo de links
+  catalog <-
+    data.frame(
+      full_url = these_links ,
+      type = paste0( "siasus_" , gsub( ".*SIASUS\\/|\\/.*|_" , "" , these_links ) ) ,
+      stringsAsFactors = FALSE
+    )
+  
+  # define subtipos a partir de 2008
+  these_subtype_codes <- toupper( basename( catalog[ catalog$type == "siasus_200801" , "full_url" ] ) )
+  these_subtype_codes <- toupper( gsub( "[A-Z]{2}[0-9]{4}[A-Z]{0,1}\\..*" , "" , these_subtype_codes ) )
+  catalog[ catalog$type == "siasus_200801" , "subtype_code" ] <- these_subtype_codes
+  
+  # de 1994-2007, só existia o código "pa"
+  catalog[ catalog$type == "siasus_199407200712" , "subtype_code" ] <- "PA"
+  
+  # cria df com descrição de subtipos
+  stype_df <- data.frame( subtype_code = c("AB", "ABO", "ACF", "AD", "AM", "AMP", "AN", "AQ", "AR", "ATD", "BI", "PA", "PS", "SAD") , 
+                          subtype = c( "bariatrica", "pos_bariatrica", "fistula_arteriovenosa", "laudos_diversos", "medicamentos", 
+                                       "multiprofissional", "nefrologia", "quimioterapia", "radioterapia", "dialitico", "bpai", 
+                                       "producao_ambulatorial", "psicossocial", "atencao_domiciliar" ) , stringsAsFactors = FALSE )
+  
+  # descarta subtipos sem descrição
+  stype_df <- stype_df[ !is.na( stype_df$subtype ) , ]
+  
+  # combina com catalogo
+  catalog <- merge( catalog , stype_df , by = "subtype_code" , all.x = TRUE )
+  
+  # define mês e ano
+  these_timestamps  <- gsub( "[^0-9]" , "" , basename( catalog[ , "full_url" ] ) )
+  these_years <- as.numeric( substr( these_timestamps , 1 , 2 ) )
+  these_years <- ifelse( these_years > 90 , 1900 + these_years , 2000 + these_years )
+  these_months <- as.numeric( substr( these_timestamps , 3 , 4 ) )
+  catalog[ , "year" ] <- these_years
+  catalog[ , "month" ] <- these_months
+  
+  # coleta ufs
+  these_ufs  <- gsub( "[0-9].*" , "" , toupper( basename( catalog[ , "full_url" ] ) ) )
+  these_ufs <- ifelse( nchar( these_ufs ) > 4 , substr( these_ufs , 4 , 5 ) , substr( these_ufs , 3 , 4 ) )
+  catalog[ , "uf" ] <- these_ufs 
+  
+  # define nome do arquivo destino
+  catalog[ , "output_filename" ] <- 
+    file.path( output_dir , 
+               catalog[ , "type" ] , 
+               catalog[ , "subtype" ] , 
+               gsub( "\\.db(c|f)$" , ".fst" , tolower( basename( catalog[ , "full_url" ] ) ) ) )
+  
+  # reordena catálogo
+  catalog <- catalog[ order( catalog[ , "subtype" ] , catalog[ , "year" ] , catalog[ , "uf" ] ) , ]
+  
+  # corrige numero de linhas
+  row.names(catalog) <- NULL
+  
+  # retorna catálogo
+  catalog
+  
+}
+
+# cria datavault
+datavault_siasus <- function( catalog , datavault_dir , skipExist = TRUE ) {
+  
+  # carrega libraries
+  library(future.apply)
+  
+  # define processamento paralelo
+  plan(multiprocess)
+  
+  # define pastas do datavault
+  catalog[ , "datavault_file" ] <- gsub( "ftp://ftp.datasus.gov.br/dissemin/publicos" , datavault_dir , catalog$full_url , fixed = TRUE )
+  catalog[ , "datavault_file" ] <- gsub( "//" , "/" , catalog[ , "datavault_file" ] , fixed = TRUE )
+  
+  # procura arquivos existentes
+  existing_files <- file.exists( catalog[ , "datavault_file" ] )
+  
+  # baixa arquivos
+  future_lapply( seq_along( catalog$full_url ) , function( this_entry ) {
+    
+    # pula arquivos existentes
+    if ( file.exists( catalog[ this_entry , "datavault_file" ] ) ){ cat( "file" , this_entry , "out of" , nrow( catalog ) , "downloaded to" , datavault_dir , "\r") ; return( NULL ) }
+    
+    # cria pasta destino
+    dir.create( dirname( catalog[ this_entry , "datavault_file" ] ) , recursive = TRUE , showWarnings = FALSE )
+    
+    # baixa arquivo
+    download.file( catalog[ this_entry , "full_url" ], catalog[ this_entry , "datavault_file" ] , mode = "wb" , quiet = TRUE )
+    
+    # acompanhamento de processo
+    cat( "file" , this_entry , "out of" , nrow( catalog ) , "downloaded to" , datavault_dir , "\r")
+    
+  } )
+  
+  # mostra mensagem
+  cat( "\nsiasus datavault was built at" , datavault_dir , "\n" )
+  
+  # returna catálogo
+  catalog
+  
+}
+
+
+### monta bases de dados
+build_siasus <- function( catalog , skipExist = TRUE ) {
+  
+  # carrega pacotes
+  library(data.table)
+  library(read.dbc)
+  library(fst)
+  library(stringr)
+  
+  # cria arquivo temporário
+  tf <- tempfile()
+  
+  # "circula" pelas entradas do catálogo
+  for ( i in seq_len( nrow( catalog ) ) ){
+    
+    # pula arquivo existente
+    if (skipExist & file.exists( catalog[ i , "output_filename"] ) ) {
+      catalog[ i , 'case_count' ] <- fst.metadata( catalog[ i , "output_filename"] )$nrOfRows
+      cat( paste0( catalog[ i , 'output_filename' ] , " already exists. Skippping.\r" ) )
+      next()
+    } 
+    
+    # baixa arquivo de dados
+    if ( is.null( catalog[ i , "datavault_file" ] ) ) {
+      download.file( catalog[ i , "full_url" ] , tf , quiet = TRUE , mode = "wb" )
+    } else if ( is.na( catalog[ i , "datavault_file" ] ) ) {
+      download.file( catalog[ i , "full_url" ] , tf , quiet = TRUE , mode = "wb" ) 
+    } else {
+      file.copy( catalog[ i , "datavault_file" ] , tf , overwrite = TRUE )
+    }
+    
+    # lê arquivo .dbc
+    x <- read.dbc( tf , as.is = TRUE )
+    
+    # converte para data.table
+    x <- data.table( x )
+    
+    # nomes das colunas em minúsculo
+    names( x ) <- tolower( names( x ) )
+    
+    # remove espaços desnecessários nos nomes
+    names( x ) <- trimws( names( x ) , which = "both" )
+    
+    # força variáveis de códigos para caractere
+    code_vars <- names( x )[ grepl( "(munic|uf)" , names( x ) ) ]
+    x[ , (code_vars) := lapply( .SD , as.character ) , .SDcols = code_vars ]
+    
+    # # ajusta datas
+    # date_vars <- names( x )[ grepl( "(dt)_" , names( x ) ) ]
+    # x[ , (date_vars) := lapply( .SD , function(z) as.Date( z , format = "%YYYY-%MM-%DD" ) ) , .SDcols = date_vars ]
+    # x[ , (date_vars) := lapply( .SD , as.character ) , .SDcols = date_vars ]
+    
+    # salva contagem de casos
+    catalog[ i , 'case_count' ] <- nrow( x )
+    
+    # salva arquivo no disco
+    dir.create( dirname( catalog[ i , 'output_filename' ] ) , recursive = TRUE , showWarnings = FALSE )
+    if ( nrow(x) > 0 ) write_fst( x , path = catalog[ i , 'output_filename' ] , compress = 100 )
+    
+    # acompanhamento de processo
+    cat( paste0( "siasus catalog entry " , i , " of " , nrow( catalog ) , " stored at '" , catalog[ i , 'output_filename' ] , "'\r" ) )
+    
+    # deleta arquivo temporário
+    suppressWarnings( file.remove( tf ) )
+    
+  }
+  
+  # retorna catálogo
+  catalog
+  
+}
+
+### cria base de dados monetdb
+monetdb_siasus <- function( catalog , skipExist = TRUE ) {
+  
+  # carrega libraries
+  library(data.table)
+  library(fst)
+  library(DBI)
+  library(MonetDBLite)
+  
+  # filtra apenas para arquivos existentes
+  catalog <- catalog[ file.exists( catalog$output_filename ) , ]
+  
+  # cria nomes das tabelas na base de dados
+  catalog$db_tablename <- paste( catalog$type , catalog$subtype , sep = "_")
+  
+  # adiciona sufixo de ano
+  catalog$db_tablename <- paste( catalog$db_tablename , catalog$year , sep = "_" )
+  
+  # cria endereço da base de dados
+  catalog$dbfolder <- ifelse( is.na( catalog$db_tablename ) , NA , paste0( output_dir , "/MonetDB" ) )
+  
+  # combinações únicas de bases de dados e tabelas
+  dbentries <- unique( catalog[ , c( "dbfolder" , "db_tablename" ) ] )
+  
+  # ajusta linhas
+  rownames(dbentries) <- NULL
+  
+  # # deleta bases de dados preexistentes
+  # for ( this_folder in unique( dbentries[ , "dbfolder" ] ) ) { unlink( this_folder , recursive = TRUE ) }
+  
+  # abre conexão com a tabela
+  db <- dbConnect( MonetDBLite() , unique( dbentries[ , "dbfolder" ] )[[1]] )
+  
+  # acompanhamento de processo
+  cat( "\nsiasus monetdb process started.\n" )
+  
+  # para cada base de dados e tabela associada
+  for ( i in seq_len( nrow(dbentries) ) ) {
+    
+    # # abre conexão com a tabela
+    # db <- dbConnect( MonetDBLite() , dbentries[ i , "dbfolder" ] )
+    
+    # testa se a tabela já existe na base de dados
+    if ( dbExistsTable( db , dbentries[ i , "db_tablename" ] ) ) {
+      
+      # Se skipExist == TRUE, pula para a próxima tabela.
+      # Caso contrário, deleta tabela existente e processa os dados
+      if ( skipExist ) { 
+        cat( dbentries[ i , "db_tablename" ] , "already exists. Skipping.\r" )
+        next() 
+      } else { 
+        cat( dbentries[ i , "db_tablename" ] , "already exists. Removing.\r" )
+        dbRemoveTable( db , dbentries[ i , "db_tablename" ] ) 
+      }
+      
+    }
+    
+    # lista todos os arquivos desta tabela
+    these_files <- subset( catalog, dbfolder == dbentries[ i , "dbfolder" ] & db_tablename == dbentries[ i , "db_tablename" ] ) [ , 'output_filename' ]
+    
+    # coleta nomes e formatos de colunas em todos os arquivos desta base
+    these_dts <- lapply( these_files , function(this_file) {
+      
+      # pula arquivos inexistentes
+      if ( !file.exists( this_file ) ) return( NULL )
+      
+      # lê metadados
+      x <- read_fst( this_file , as.data.table = TRUE , from = 1 , to = 1 )
+      
+      # coleta nome de colunas
+      these_cols <- colnames( x )
+      
+      # coleta formato de colunas
+      these_formats <- sapply( x , class )
+      
+      # monta data.table
+      data.table( filename = this_file , column_name = these_cols , column_format = these_formats )
+      
+    } )
+    
+    # empilha dados
+    data_structure <- rbindlist( these_dts , use.names = TRUE )
+    
+    # reformata estrutura
+    data_structure <- dcast( data_structure , column_name ~ column_format , fill = "double" , drop = FALSE , value.var = "column_format" , fun.aggregate = unique )
+    
+    # define formato final da coluna
+    data_structure <- as.data.frame( data_structure )
+    data_structure$col_format <- apply( data_structure[ , -1 ] , 1 , function( y ) { ifelse( any( tolower(y) %in% c( "character", "date" ) ) , "character" ,"numeric" ) } )
+    
+    # cria estrutura final
+    data_structure <- data_structure[ , c( "column_name" , "col_format" ) ]
+    
+    # lê arquivo, formata colunas e salva dados na tabela da base de dados
+    for ( this_file in these_files ) {
+      
+      # pula arquivos inexistentes
+      if ( !file.exists( this_file ) ) next()
+      
+      # lê arquivo
+      x <- read_fst( this_file , as.data.table = TRUE )
+      
+      # cria colunas ausentes
+      missing_cols <- data_structure$column_name [ !is.element( data_structure$column_name , colnames(x) ) ]
+      if ( length( missing_cols ) > 0 )  x[ , (missing_cols) := NA ]
+      
+      # altera formatos
+      these_num_cols <- data_structure$column_name[ data_structure$col_format == "numeric" ]
+      x[ , (these_num_cols) := lapply( .SD , as.numeric ) , .SDcols = these_num_cols ]
+      these_char_cols <- data_structure$column_name[ data_structure$col_format == "character" ]
+      x[ , (these_char_cols) := lapply( .SD , as.character ) , .SDcols = these_char_cols ]
+      
+      # reordena colunas
+      setcolorder( x , data_structure$column_name )
+      
+      # salva dados na tabela
+      dbWriteTable( db , dbentries[ i , "db_tablename" ] , x , append = TRUE )
+      rm( x ) ; gc()
+      
+      # acompanhamento de processo
+      cat( basename( this_file ) , "stored at" , dbentries[ i , "db_tablename" ] , "\r" )
+      
+    }
+    
+    # # disconecta da base de dados
+    # dbDisconnect( db , shutdown = TRUE )
+    
+  }
+  
+  # disconecta da base de dados
+  dbDisconnect( db , shutdown = TRUE )
+  
+  # acompanhamento de processo
+  cat( "\nsiasus monetdb database was built." )
+  
+}
