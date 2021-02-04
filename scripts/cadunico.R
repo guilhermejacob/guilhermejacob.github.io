029ed43c468250ded51a3476cc99199d449d403d
guilhermejacob <guilhermejacob91@gmail.com>
2021-02-04 14:29:59 -0300

    update

--- scripts/cadunico.R
+++ scripts/cadunico.R
@@ -31,7 +31,7 @@ catalog_cadunico <-
     catalog <-
       data.frame(
         year = cadunico_years ,
-        dbfolder = paste0( output_dir , "/MonetDB" ) ,
+        dbfile = file.path( output_dir , "cadunico.db" ) ,
         output_folder = output_dir ,
         full_url = these_links ,
         is.microdata = is.microdata ,
@@ -56,8 +56,16 @@ catalog_cadunico <-
 # datavault
 datavault_cadunico <- function( catalog , datavault_dir , skipExist = TRUE ) {
   
+  # load libraries
+  library( RCurl )
+  
   # create datavault links
   catalog[ , "datavault_file" ] <- paste0( datavault_dir , "/" , gsub( ".zip.*" , ".zip" , basename( catalog[ , "full_url" ] ) ) )
+  
+  # fix url
+  these.urls <- catalog[ , "full_url" ] 
+  these.urls <- gsub( ".*l=https" , "https" , these.urls )
+  these.urls <- gsub( ".zip.*" , ".zip" , these.urls )
   
   # check for existing files
   existing_files <- file.exists( catalog[ , "datavault_file" ] )
@@ -78,7 +86,12 @@ datavault_cadunico <- function( catalog , datavault_dir , skipExist = TRUE ) {
       if ( !dir.exists( dirname( catalog[i , "datavault_file" ] ) ) ) dir.create( dirname( catalog[i , "datavault_file" ] ) , recursive = TRUE )
       
       # download file
-      download.file( catalog[ i , "full_url" ] , catalog[ i , "datavault_file" ] , quiet = FALSE )
+      file.create( catalog[i , "datavault_file" ] )
+      f <- CFILE( catalog[i , "datavault_file" ] , "wb")
+      url <- these.urls[ i ]
+      curlPerform(url = url, writedata = f@ref , .opts = list( ssl.verifypeer = FALSE ) )
+      close(f)
+      
       
       # process tracker
       cat( "file" , i , "out of" , nrow( catalog ) , "downloaded to" , datavault_dir , "\r")
@@ -96,12 +109,13 @@ datavault_cadunico <- function( catalog , datavault_dir , skipExist = TRUE ) {
 }
 
 
+# build sqlite database
 build_cadunico <-
   function( catalog ){
     
     # load libraries
     library(DBI)
-    library(MonetDBLite)
+    library(RSQLite)
     library(data.table)
     library(survey)
     
@@ -109,13 +123,14 @@ build_cadunico <-
     tf <- tempfile()
     td <- file.path( tempdir() , "unzips" )
     
+    # process data
     for ( i in seq_len( nrow( catalog ) ) ){
       
       # create directory
-      if ( !dir.exists( dirname( catalog[ i , 'dbfolder' ] ) ) ) dir.create( dirname( catalog[ i , 'dbfolder' ] ) , recursive = TRUE )
+      if ( !dir.exists( dirname( catalog[ i , 'dbfile' ] ) ) ) dir.create( dirname( catalog[ i , 'dbfile' ] ) , recursive = TRUE )
       
-      # open the connection to the monetdblite database
-      db <- dbConnect( MonetDBLite() , catalog[ i , "dbfolder" ] )
+      # open the connection to the SQLite database
+      db <- dbConnect( SQLite() , catalog[ i , "dbfile" ] )
       
       # download the file
       if ( is.null( catalog[ i , "datavault_file" ] ) ) {
@@ -129,17 +144,21 @@ build_cadunico <-
       # process tracker
       cat( "extracting files.\n")
       
+      # extracct files to temporary folder
       unzip( tf , exdir = td )
       unzipped_files <- list.files( td , full.names = TRUE , recursive = TRUE )
       
+      # platform specific
       if( .Platform$OS.type != 'windows' ){
         sapply(unique(dirname(gsub( "\\\\" ,  "/" , unzipped_files ))),dir.create,showWarnings=FALSE)
         file.rename( unzipped_files , gsub( "\\\\" ,  "/" , unzipped_files ) )
         unzipped_files <- gsub( "\\\\" ,  "/" , unzipped_files )
       }
       
+      # extract ffile
       data_files <- grep( "\\.csv$", unzipped_files, value = TRUE , ignore.case = TRUE )
       
+      # read data
       for ( this_table_type in c( "familia" , "pessoa" ) ) {
         
         # process tracker
@@ -149,19 +168,47 @@ build_cadunico <-
         this_data_file <- data_files [ grepl( this_table_type , basename( data_files ) , ignore.case = TRUE ) ]
         
         # read data set
-        csvdata <- fread( this_data_file , sep = ";" , dec = "," , showProgress = FALSE , data.table = TRUE )
+        if ( catalog[ i , "year" ] == 2018 ) {
+          csvdata <- fread( this_data_file , sep = ";" , dec = "," , 
+                            colClasses = list( "character" = c( "cd_ibge" , "id_familia" , "id_pessoa" , "peso.fam" , "peso.pes" ) ) ,
+                            keepLeadingZeros = TRUE ,
+                            showProgress = FALSE , data.table = TRUE ) 
+        } else {
+          csvdata <- fread( this_data_file , sep = ";" , dec = "," , 
+                            colClasses = list( "character" = c( "cd_ibge" , "id_familia" , "id_pessoa" ) ) ,
+                            keepLeadingZeros = TRUE ,
+                            showProgress = FALSE , data.table = TRUE )
+        }
         
         # fix column names
-        column.names <- colnames( csvdata )
-        column.names <- gsub( '\"' , "" , column.names ) # remove quotes
-        column.names <- gsub( '\\.' , "_" , column.names ) # remove dots
-        colnames( csvdata ) <- column.names
+        csvdata <- janitor::clean_names( csvdata )
+        # column.names <- colnames( csvdata )
+        # column.names <- gsub( '\"' , "" , column.names ) # remove quotes
+        # column.names <- gsub( '\\.' , "_" , column.names ) # remove dots
+        # colnames( csvdata ) <- column.names
+        
+        # adjust 2018 family weights
+        if ( catalog[ i , "year" ] == 2018 ) {
+          csvdata[ , peso_fam := stringr::str_pad( peso_fam , 15 , "right" , 0 ) ]
+          csvdata[ , peso_fam := as.numeric( peso_fam ) * 1e-14 ]
+        } 
+        
+        # adjust 2018 person weights
+        if ( catalog[ i , "year" ] == 2018 & this_table_type == "pessoa" ) {
+          csvdata[ , peso_pes := stringr::str_pad( peso_pes , 15 , "right" , 0 ) ]
+          csvdata[ , peso_pes := as.numeric( peso_pes ) * 1e-14 ]
+        } 
         
         # add column of ones
         csvdata[ , one := 1 ]
         
+        # convert date to strings
+        date.cols <- sapply( sapply( csvdata , class ) , function(z) "Date" %in% z )
+        date.cols <- colnames( csvdata ) [ date.cols ]
+        csvdata[ , c( date.cols ) := lapply( .SD , as.character ) , .SDcols = date.cols ]
+        
         # store records in table
-        dbWriteTable( db , paste0( this_table_type , "_" , catalog[ i , "year" ] ) , csvdata )
+        dbWriteTable( db , paste0( this_table_type , "_" , catalog[ i , "year" ] ) , csvdata , overwrite = TRUE )
         
         # drop objects and delete file
         rm( csvdata ) ; file.remove( this_data_file ) ; gc()
@@ -169,36 +216,7 @@ build_cadunico <-
         # process tracker
         cat( tolower( gsub( "\\..*" , "" , basename( this_data_file ) ) ) , "stored at" , paste0( this_table_type , "_" , catalog[ i , "year" ] ) , "\n" )
         
-        # # create and merge fpc
-        # {
-        #   # create fpc table
-        #   table_fpc_query <-
-        #     paste0( "CREATE TABLE " ,
-        #             ifelse( this_table_type == "pessoa" , "pes" , "fam" ) ,
-        #             "_fpc AS ( SELECT estrato , SUM( ",
-        #             ifelse( this_table_type == "pessoa" , "peso_pes" , "peso_fam" ) ,
-        #             " ) as " , ifelse( this_table_type == "pessoa" , "pes" , "fam" ) , "_fpc FROM " ,
-        #             this_table_type ,"_" , catalog[ i , 'year' ] ,
-        #             " GROUP BY estrato ) "
-        #     )
-        #   dbSendQuery( db , table_fpc_query )
-        #
-        #   fpc_merge_query <- paste0(
-        #     'create table cadunico_' ,
-        #     catalog[ i , 'year' ] ,
-        #     ' as (select a.* , ' , paste( paste0( "b." , setdiff( pes_cols , fam_cols ) ) , collapse = " , " ) ,
-        #     ' from familia_' ,
-        #     catalog[ i , 'year' ] ,
-        #     ' as a inner join pessoa_' ,
-        #     catalog[ i , 'year' ] ,
-        #     ' as b on a.estrato = b.id_familia' ,
-        #     ') WITH DATA'
-        #   )
-        #
-        #   # add fpc variables to the main table
-        #
-        # }
-        
+        # process person table
         if (this_table_type == "pessoa") {
           
           # process tracker
@@ -244,7 +262,7 @@ build_cadunico <-
         
         # process tracker
         cat( "building survey designs.\n")
-        
+
         # create family design
         this_design <-
           svydesign(
@@ -253,18 +271,17 @@ build_cadunico <-
             weight = ~peso_fam ,
             nest = TRUE ,
             data = paste0( "familia_" , catalog[ i , "year" ] ) ,
-            dbtype = "MonetDBLite" ,
-            dbname = catalog[ i , 'dbfolder' ]
+            dbtype = "SQLite" ,
+            dbname = catalog[ i , 'dbfile' ]
           )
-        
+
         # save survey design
         saveRDS( this_design , file = file.path( output_dir , paste0( "familia " , catalog[ i , "year" ] , " design.rds" ) ) )
-        rm( this_design ) 
-        
+        rm( this_design )
+
         # process tracker
-        # cat( paste0( data_name , " survey design entry " , i , " of " , nrow( unique_designs ) , " stored at '" , unique_designs[ i , 'design' ] , "'\r\n\n" ) )
         cat( paste0( "familia " , catalog[ i , "year" ] ) , "design stored" , "\n" )
-        
+
         # create person design
         this_design <-
           svydesign(
@@ -273,14 +290,14 @@ build_cadunico <-
             weight = ~peso_pes ,
             nest = TRUE ,
             data = paste0( "cadunico_" , catalog[ i , "year" ] ) ,
-            dbtype = "MonetDBLite" ,
-            dbname = catalog[ i , 'dbfolder' ]
+            dbtype = "SQLite" ,
+            dbname = catalog[ i , 'dbfile' ]
           )
-        
+
         # save design
         saveRDS( this_design , file = file.path( output_dir , paste0( "cadunico " , catalog[ i , "year" ] , " design.rds" ) ) )
         rm( this_design )
-        
+
         # process tracker
         # cat( paste0( data_name , " survey design entry " , i , " of " , nrow( unique_designs ) , " stored at '" , unique_designs[ i , 'design' ] , "'\r\n\n" ) )
         cat( paste0( "cadunico " , catalog[ i , "year" ] ) , "design stored" , "\n" )
@@ -288,7 +305,7 @@ build_cadunico <-
       }
       
       # store case count
-      catalog[ i , 'case_count' ] <- dbGetQuery( db , paste0( "SELECT sum(peso_pes) FROM pessoa_" , catalog[ i , "year" ] ) )[ 1 , 1 ]
+      catalog[ i , 'case_count' ] <- dbGetQuery( db , paste0( "SELECT sum( peso_pes ) FROM pessoa_" , catalog[ i , "year" ] ) )[ 1 , 1 ]
       
       # disconnect from the current monet database
       dbDisconnect( db , shutdown = TRUE )
@@ -298,7 +315,7 @@ build_cadunico <-
       unlink( td , recursive = TRUE )
       
       # process tracker
-      cat( paste0( "cadunico catalog entry " , i , " of " , nrow( catalog ) , " stored in '" , catalog[ i , 'dbfolder' ] , "'\n\n" ) )
+      cat( paste0( "cadunico catalog entry " , i , " of " , nrow( catalog ) , " stored in '" , catalog[ i , 'dbfile' ] , "'\n\n" ) )
       
     }
     
